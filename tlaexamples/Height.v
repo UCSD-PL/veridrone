913342f533b1901db143d03f2b443e037145bdd2
Daniel Ricketts <daniel.bmore.ricketts@gmail.com>
2014-10-24 02:30:13 +0200

    Cleaned up the proof of the height controller.

--- tlaexamples/Height.v
+++ tlaexamples/Height.v
@@ -12,6 +12,10 @@ Open Scope string_scope.
 Section HeightCtrl.
 
   Variable d : R.
+  (* The following hypothesis is not necessary
+     for the safety property, but it's necessary
+     for ensuring that non-Zeno behaviors are
+     possible. *)
   Hypothesis Hd : (d > 0)%R.
 
   Definition Read : Formula :=
@@ -62,53 +66,6 @@ Section HeightCtrl.
   Lemma ind_inv_safe : |- Ind_Inv --> Safe.
   Proof. solve_linear. Qed.
 
-Ltac find_zeros eqs :=
-  match eqs with
-    | nil => constr:(@nil Var)
-    | cons (DiffEqC ?y (ConstC (NatC O))) ?eqs =>
-      let rest := find_zeros eqs in
-      constr:(cons y rest)
-    | cons _ ?eqs =>
-      let rest := find_zeros eqs in
-      rest
-  end.
-
-Ltac extract_unchanged eqs :=
-  let xs := find_zeros eqs in
-  let rec aux l :=
-      match l with
-        | nil => idtac
-        | cons ?y ?l => apply zero_deriv
-                        with (cp:=eqs) (x:=y);
-                        try (aux l)
-      end in
-  aux xs.
-
-Ltac get_var_inv F x :=
-  match F with
-    | And ?F1 _ =>
-      get_var_inv F1 x
-    | And _ ?F2 =>
-      get_var_inv F2 x
-    | Comp (next_term x) (next_term ?e) Eq => constr:(Comp x e Eq)
-  end.
-
-Ltac get_inv eqs F :=
-  let xs := find_zeros eqs in
-  let rec aux l :=
-      match l with
-        | nil => constr:(TRUE)
-        | cons ?y ?l =>
-          let y := constr:(TermC y) in
-          let vinv := get_var_inv F y in
-          let rest := aux l in
-          constr:(And vinv rest)
-        | cons _ ?l =>
-          let rest := aux l in
-          rest
-      end in
-  aux xs.
-
   Lemma safety :
     |- (Init /\ []Next) --> []Safe.
   Proof.
@@ -118,100 +75,34 @@ Ltac get_inv eqs F :=
       + apply and_left2. apply imp_id.
     - apply imp_trans with (F2:=[]Ind_Inv).
       + apply inv_discr_ind; auto. unfold Next, Evolve.
-        repeat apply or_next;
+        Time repeat apply or_next;
+          repeat first [ apply and_right |
+                         apply imp_right ];
         match goal with
           | [ |- context [Continuous ?eqs] ]
-              => pose "Continuous"; repeat apply and_right;
-                 try solve [extract_unchanged eqs; solve_linear]
+              => pose "Continuous"; extract_unchanged eqs;
+                 match goal with
+                   | [ |- context [next_term (TermC (VarC "pc")) =
+                                   next_term (TermC (ConstC (NatC 1)))] ] =>
+                     match goal with
+                       | [ |- context [next_term (TermC (VarC "v")) = next_term ?e] ] =>
+                         abstract (prove_diff_inv ("v" = e);
+                                   match goal with
+                                     | [ |- (|- (?I /\ Continuous eqs) --> next ?I) ] =>
+                                       extract_unchanged eqs; solve_linear
+                                     | [ |- _ ] =>
+                                       solve_linear
+                                   end)
+                     end
+                   | [ |- _ ] =>
+                     try abstract solve [solve_linear |
+                                         prove_diff_inv TRUE; solve_linear]
+                 end
           | [ |- _ ]
               => pose "Discrete";
-                 repeat first [ apply and_right |
-                                apply imp_right ];
-                 try solve_linear
+                 try abstract solve_linear
         end.
-
-Ltac prove_diff_inv known :=
-  match goal with
-      |- context [ Continuous ?eqs ] =>
-      match goal with
-          |- (|- _ --> Comp (next_term ?t1) (next_term ?t2) ?op) =>
-          apply diff_ind with
-          (Hyps:=known) (G:=Comp t1 t2 op) (cp:=eqs); try solve [solve_linear]
-      end
-(*      => apply diff_ind with
-         (Hyps:=TRUE) (G:=Comp t1 t2 op) (cp:=eqs); solve_linear*)
-  end.
-
-(*Ltac assert_base_diff_inv t1 t2 op :=
-  match goal with
-      |- context [ Continuous ?eqs ]
-      => assert (|- (Comp t1 t2 op /\ Continuous eqs) --> Comp (next_term t1) (next_term t2) op);
-         try (prove_diff_inv TRUE)(*try (apply diff_ind with
-              (Hyps:=TRUE) (G:=Comp t1 t2 op) (cp:=eqs); solve_linear)*)
-  end.*)
-repeat first [ apply and_right |
-               apply imp_right ].
-prove_diff_inv TRUE.
-prove_diff_inv TRUE.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-prove_diff_inv ("v" = 1).
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-prove_diff_inv ("v" = 1).
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-repeat first [ apply and_right |
-               apply imp_right ].
-prove_diff_inv TRUE.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-prove_diff_inv TRUE.
-prove_diff_inv ("v" = --1).
-(* Should apply diff_ind here instead. *)
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-prove_diff_inv ("v" = --1).
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-match goal with
-|- context [Continuous ?eqs] =>
-  extract_unchanged eqs
-end; solve_linear.
-prove_diff_inv TRUE.
-+ apply always_imp. apply ind_inv_safe.
-Qed.
+      + apply always_imp. apply ind_inv_safe.
+  Qed.
 
 End HeightCtrl.
