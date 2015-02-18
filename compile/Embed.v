7f178456569285902e1b71b1da1f9829b37fd5a9
Daniel Ricketts <daniel.bmore.ricketts@gmail.com>
2015-02-18 05:29:33 +0100

    Improved definition of embedding C in TLA.

--- compile/Embed.v
+++ compile/Embed.v
@@ -18,31 +18,18 @@ Section embedding.
   (** The standard evaluation relation **)
   Variable eval : state -> ast -> state -> Prop.
 
-  (** In the state, the variable stands for the given real **)
-  Variable VarRelated : state -> var -> R -> Prop.
   (** In the state, the variable is represented as the returned real **)
   Variable asReal : state -> var -> R.
 
-  (** The program state is an abstraction of the TLA state **)
-  Fixpoint models (vars : list (Syntax.Var * var))
-  : Syntax.state -> state -> Prop :=
-    match vars with
-    | nil => fun _ _ => True
-    | (v,v') :: vars =>
-      let P := models vars in
-      fun tla_state st =>
-        VarRelated st v' (tla_state v) /\ P tla_state st
-    end.
-
-  (** On the output, the program gets to write exactly the given values to the
-   ** TLA state, so this function computes the new final state using [asReal]
+  (** This states that the value in the TLA state is exactly
+   ** equal to the value in the program state.
    **)
-  Fixpoint modelsOutput (vars : list (Syntax.Var * var))
+  Fixpoint models (vars : list (Syntax.Var * var))
            (tla_st : Syntax.state) (st : state) : Prop :=
     match vars with
     | nil => True
     | (v,v') :: vars =>
-      tla_st v = asReal st v' /\ modelsOutput vars tla_st st
+      tla_st v = asReal st v' /\ models vars tla_st st
     end.
 
   (** Running the given program in the current state. Only the specified
@@ -53,6 +40,6 @@ Section embedding.
                     exists init_state post_state : state,
                       models vars pre init_state /\
                       eval init_state prg post_state /\
-                      modelsOutput vars post post_state).
+                      models vars post post_state).
 
 End embedding.
