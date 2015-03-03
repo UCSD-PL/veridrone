638d1f746df7e9d5f2a5606075ef1d5c242551df
Daniel Ricketts <daricket@cs.ucsd.edu>
2015-03-03 15:03:18 -0800

    Added general definition of hybrid systems, an induction theorem, a composition theorem, and a sensor example.

--- tlaexamples/OneDimVelShim.v
+++ tlaexamples/OneDimVelShim.v
@@ -11,6 +11,166 @@ Require Import Coq.Reals.Rdefinitions.
 Open Scope HP_scope.
 Open Scope string_scope.
 
+Definition World (world : list DiffEq) (d : R) : Formula :=
+  Continuous (("t"'::=--1)::world) /\ "t"! >= 0 /\ "pc" = 0.
+
+Definition Discr (Prog : Formula) (d : R) : Formula :=
+  Prog /\ "t"! = d /\ "pc" = 1.
+
+Definition Sys (Init Prog: Formula) (world : list DiffEq)
+           (d : R) : Formula :=
+  Init /\ [](World world d \/ Discr Prog d).
+
+Theorem sys_by_induction : forall Init Prog Inv IndInv w d,
+  is_st_formula IndInv ->
+  (|- Init --> IndInv) ->
+  (|- IndInv --> Inv) ->
+  (|- (IndInv /\ World w d) --> next IndInv) ->
+  (|- (IndInv /\ Discr Prog d) --> next IndInv) ->
+  (|- Sys Init Prog w d --> [] Inv).
+Proof.
+  intros Init Prog Inv IndInv w d Hst Hinit Hinv Hw Hdiscr.
+  apply imp_trans with (F2:=[]IndInv).
+  - apply imp_trans with (F2:=Sys IndInv Prog w d).
+    + unfold Sys. apply and_right.
+      * apply and_left1; assumption.
+      * apply and_left2; apply imp_id.
+    + apply inv_discr_ind.
+      * assumption.
+      * apply or_next; assumption.
+  - apply always_imp; assumption.
+Qed.
+
+Section SenseCtrl.
+
+  Variable Is : Formula.
+  Variable Ic : Formula.
+  Variable w : list DiffEq.
+  Variable d : R.
+  Variable S : Formula.
+  Variable C : Formula.
+  Variable P : Formula.
+  Variable E : Formula.
+
+  Theorem sense_ctrl :
+    (|- Sys Is S w d --> []E) ->
+    (|- Sys Ic (C /\ E) w d --> []P) ->
+    (|- Sys (Is /\ Ic) (S /\ C) w d --> [](P /\ E)).
+  Proof.
+    Opaque World Discr.
+    simpl. intros Hs Hc tr [ [HIs HIc] HN] n.
+    split.
+    - apply Hc. intuition.
+      pose proof (HN n0). intuition.
+      right. Transparent Discr. revert H0.
+      unfold Discr. simpl. intuition.
+      apply Hs. intuition.
+      specialize (HN n1). intuition.
+      right. simpl in *. intuition.
+    - apply Hs. intuition.
+      specialize (HN n0). intuition.
+      right. simpl in *. intuition.
+  Qed.
+
+End SenseCtrl.
+
+Section SensorWithError.
+
+  Variable x : Var.
+  Variable Xmax : Var.
+  Variable Xmin : Var.
+  Variable err : R.
+
+  Definition Sense : Formula :=
+    Xmax = Xmin + err /\ Xmin <= x <= Xmax.
+
+  Definition SenseSafe : Formula :=
+    "pc" = 1 --> Xmin <= x <= Xmax.
+
+  Theorem sense_safe : forall w d,
+    |- Sys SenseSafe Sense w d --> []SenseSafe.
+  Proof.
+    intros w d. apply and_left2.
+    apply always_imp. solve_linear.
+  Qed.
+
+End SensorWithError.
+
+Section Ctrl.
+
+  (* Upper bound on velocity. *)
+  Variable ub : R.
+  (* Max time between executions of the
+     controller. *)
+  Variable d : R.
+  Hypothesis d_gt_0 : (d > 0)%R.
+  
+  (* The continuous dynamics of the system *)
+  Definition world : list DiffEq :=
+    ["v"' ::= "a", "a"' ::= 0].
+
+  Definition Ctrl : Formula :=
+       ("A"*d + "Vmax" <= ub /\ "a"! = "A")
+    \/ ("a"! <= 0).
+
+(*
+  Definition Ctrl1 : Formula :=
+       ("A"*d + "v" <= ub /\ "a"! = "A")
+    \/ ("a"! <= 0).
+
+  Definition Ctrl2 : Formula :=
+       "V"! = "v"
+    /\   (("a" >= 0 /\ "A"*d + "V" + "a"*d <= ub /\ "a"! = "A")
+       \/ ("a" < 0 /\ "A"*d + "V" <= ub /\ "a"! = "A")
+       \/ "a"! <= 0).
+
+  Definition Next (Ctrl:Formula) : Formula :=
+       (Evolve /\ "t"! >= 0)
+    \/ (Ctrl /\ "t"! = d /\ Unchanged (["v"])).
+*)
+
+  Definition Ic : Formula :=
+    "v" <= ub /\ "v" + "a"*d <= ub /\
+    0 <= "t" <= d /\ "Vmax" >= "v".
+
+  Definition Safe : Formula :=
+    "v" <= ub.
+
+  Definition IndInv : Formula :=
+       ("a" <  0 --> Safe)
+    /\ ("a" >= 0 --> "a"*"t" + "v" <= ub)
+    /\ 0 <= "t" <= d.
+
+  Theorem ctrl_safe :
+    |- Sys Ic (Ctrl /\ "Vmax" >= "v") world d --> []"v" <= ub.
+  Proof.
+    
+
+(*
+  Definition AbstractSys : Formula :=
+    Init /\ [](Next AbstractCtrl).
+
+  Definition Ctrl1Sys : Formula :=
+    Init /\ [](Next Ctrl1).
+
+  Definition Ctrl2Sys : Formula :=
+    Init /\ [](Next Ctrl2).
+*)
+
+  Definition Safe : Formula :=
+    "v" <= ub.
+
+  Lemma ctrl_safe :
+    |- Sys Ic (Ctrl /\ VBound) w d --> []Safe.
+
+  Definition IndInv : Formula :=
+       ("a" <  0 --> Safe)
+    /\ ("a" >= 0 --> "a"*"t" + "v" <= ub)
+    /\ 0 <= "t" <= d.
+
+  Theorem abstract_safety :
+    |- AbstractSys --> []Safe.
+
 Section Ctrl.
 
   (* Upper bound on velocity. *)
