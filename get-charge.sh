9b57340e9744c4e079f4e40acfa7d12a7fd911f2
Gregory Malecha <gmalecha@cs.harvard.edu>
2015-03-05 14:33:23 -0800

    updated version of get-charge.sh

--- get-charge.sh
+++ get-charge.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 git clone git@github.com:jesper-bengtson/Charge.git
-(cd Charge; git checkout a21ed562baf247c8bc5677fc55a594325799e193; make -j3)
+(cd Charge; git checkout a21ed562baf247c8bc5677fc55a594325799e193)
+cp Charge.vars Charge/Charge\!/CoqoonMakefile.vars
+(cd Charge/Charge\!/ ; ln -s CoqoonMakefile Makefile ; make -j3)
