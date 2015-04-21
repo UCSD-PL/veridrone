77f0a70f8975ca577b6ffb1a97406c4482828be8
vigneshgowda9 <vngowda@eng.ucsd.edu>
2015-04-21 01:56:01 -0700

    removing z3 use

--- compile/bound.v
+++ compile/bound.v
@@ -6460,13 +6460,10 @@ clear H1. psatz R. Qed.
         {
           split.
           {
-            z3 solve_dbg.
             psatz R.
            }
           destruct Rcase_abs.
-          z3 solve_dbg.
           psatz R.
-          z3 solve_dbg.
           psatz R.
         }
       }
