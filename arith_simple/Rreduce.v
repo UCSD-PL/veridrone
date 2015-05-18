318b1f4623bdac8e86e6b9c3002acea300ae76f0
Gregory Malecha <gmalecha@cs.harvard.edu>
2015-05-18 16:42:41 -0700

    finished the proof of the simplification procedure.

--- arith_simple/Rreduce.v
+++ arith_simple/Rreduce.v
@@ -57,6 +57,15 @@ Section denote.
     { destruct (eq_nat_dec n n0); congruence. }
   Qed.
 
+  Fixpoint Rexpr_well_formed (r : Rexpr) : Prop :=
+    match r with
+    | Plus a b => Rexpr_well_formed a /\ Rexpr_well_formed b
+    | Mult a b => Rexpr_well_formed a /\ Rexpr_well_formed b
+    | Inv a => (RexprD a <> 0)%R /\ Rexpr_well_formed a
+    | Opp a => Rexpr_well_formed a
+    | _ => True
+    end.
+
   Definition simplify_hd (r : Rexpr) : Rexpr :=
     match r with
     | Plus r Zero => r
@@ -66,19 +75,23 @@ Section denote.
     | Mult One r => r
     | Mult r One => r
     | Opp (Opp r) => r
-(*     | Inv (Inv r) => r  NOT VALID if r = 0 *)
+    | Inv (Inv r) => r
     | _ => r
     end.
 
   Require Import Coq.micromega.Psatz.
 
   Lemma simplify_hd_sound : forall r,
-      RexprD r = RexprD (simplify_hd r).
+      Rexpr_well_formed r ->
+      RexprD (simplify_hd r) = RexprD r /\
+      Rexpr_well_formed (simplify_hd r).
   Proof.
     destruct r; simpl; auto.
-    { destruct r1; destruct r2; simpl; auto; psatzl R. }
-    { destruct r1; destruct r2; simpl; auto; psatzl R. }
-    { destruct r; simpl; psatzl R. }
+    { destruct r1; destruct r2; simpl; auto; split; solve [ tauto | psatzl R ]. }
+    { destruct r1; destruct r2; simpl; auto; split; solve [ tauto | psatzl R ]. }
+    { destruct r; simpl; intros; split; try solve [ tauto | psatzl R ].
+      rewrite RIneq.Rinv_involutive; eauto. tauto. }
+    { destruct r; simpl; split; solve [ tauto | psatzl R ]. }
   Qed.
 
   Definition lcm_factor (l r : Rexpr) : Rexpr * Rexpr * Rexpr :=
@@ -86,21 +99,30 @@ Section denote.
 
   Lemma lcm_factor_sound : forall l r d lf rf,
       lcm_factor l r = (d, lf, rf) ->
+      Rexpr_well_formed l ->
+      Rexpr_well_formed r ->
+      (RexprD l <> 0)%R ->
+      (RexprD r <> 0)%R ->
       (RexprD d = RexprD lf * RexprD l /\
-       RexprD d = RexprD rf * RexprD r)%R.
+       RexprD d = RexprD rf * RexprD r)%R /\
+      Rexpr_well_formed d /\ Rexpr_well_formed lf /\ Rexpr_well_formed rf /\
+      (RexprD d <> 0)%R /\ (RexprD lf <> 0)%R /\ (RexprD rf <> 0)%R.
   Proof.
     unfold lcm_factor. intros.
     generalize (Rexpr_eq_sound l r).
     destruct (Rexpr_eq l r); intros.
     { inversion H; clear H; subst.
-      specialize (H0 eq_refl). subst.
-      simpl. split; psatzl R. }
-    { clear H0.
+      specialize (H4 eq_refl). subst.
+      simpl. repeat split; try solve [ tauto | try psatzl R ]. }
+    { clear H4.
       generalize (simplify_hd_sound (Mult l r)).
       generalize dependent (simplify_hd (Mult l r)).
       intros. inversion H; clear H; subst.
       simpl in *.
-      rewrite <- H0. split; psatzl R. }
+      destruct H4; [ tauto | ].
+      rewrite <- H.
+      repeat split; try tauto. psatzl R. rewrite H.
+      eapply RIneq.Rmult_integral_contrapositive; tauto. }
   Qed.
 
   Fixpoint simplify (r : Rexpr) : Rexpr * Rexpr :=
@@ -114,7 +136,7 @@ Section denote.
     | Mult l r =>
       let (a,b) := simplify l in
       let (c,d) := simplify r in
-      (simplify_hd (Mult a c), simplify_hd (Mult c d))
+      (simplify_hd (Mult a c), simplify_hd (Mult b d))
     | Inv r => let (a,b) := simplify r in (b,a)
     | Opp r => let (a,b) := simplify r in (Opp a, b)
     | Zero => (Zero, One)
@@ -124,29 +146,86 @@ Section denote.
 
   Theorem simplify_sound : forall r n d,
       simplify r = (n,d) ->
-      (RexprD r = RexprD n * / RexprD d)%R.
+      Rexpr_well_formed r ->
+      (RexprD r = RexprD n * / RexprD d)%R /\
+      Rexpr_well_formed n /\ Rexpr_well_formed d /\ (RexprD d <> 0)%R.
   Proof.
-    generalize simplify_hd_sound.
-    generalize lcm_factor_sound.
     unfold simplify.
+    generalize simplify_hd_sound.
     generalize dependent simplify_hd.
+    intros simplify_hd Hsimplify_hd_sound.
+    generalize lcm_factor_sound.
     generalize dependent lcm_factor.
-    induction r0; simpl; intros;
+    intros lcm_factor Hlcm_factor_sound.
+    induction r; simpl; intros;
     repeat match goal with
+           | H : _ /\ _ |- _ => destruct H
+           | H : (_,_) = (_,_) |- _ => inversion H; clear H; subst
            | H : forall x y, ?X = (_,_) -> _
            , H' : (match ?X with (_,_) => _ end) = _ |- _ =>
-             destruct X; specialize (H _ _ eq_refl); try rewrite H
-           end.
-    { admit. }
-    { inversion H1; clear H1; subst.
-      repeat rewrite <- H0.
-      simpl. (** NOT SOUND when the result is 0 **)
-      admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
-    { admit. }
+             destruct X; specialize (H _ _ eq_refl);
+             let Hx := fresh in destruct H as [ Hx ? ] ;
+               [ tauto | try rewrite Hx ]
+           | H : match lcm_factor ?X ?Y with (_,_) => _ end = _ |- _ =>
+             generalize (Hlcm_factor_sound X Y) ;
+               destruct (lcm_factor X Y) as [ [ ? ? ] ? ] ;
+               let Hx := fresh in
+               intro Hx ;
+                 destruct (Hx _ _ _ eq_refl) ; [ tauto | tauto | tauto | tauto | clear Hx ]
+           | |- context [ simplify_hd ?X ] =>
+             let Hx := fresh in
+             destruct (Hsimplify_hd_sound X) as [ Hx ? ] ;
+               [ simpl ; tauto
+               | generalize dependent (simplify_hd X); intros ]
+           | H : _ = _ |- _ => rewrite H in *
+           end; try (split; [ simpl; try psatzl R
+                            | try tauto ]).
+    { simpl in *.
+      rewrite H19; clear H19.
+      rewrite H; clear H.
+      Lemma mult_both_eq : forall a b c : R,
+          (c <> 0 ->
+          a * c = b * c ->
+          a = b)%R.
+      Proof. intros. psatz R. Qed.
+      eapply mult_both_eq. eapply H14.
+      rewrite RIneq.Rmult_plus_distr_r.
+      repeat rewrite Raxioms.Rmult_assoc.
+      rewrite <- RIneq.Rinv_l_sym; eauto.
+      rewrite <- H11 at 2.
+      repeat rewrite <- Raxioms.Rmult_assoc.
+      clear - H5 H9.
+      symmetry. rewrite Raxioms.Rmult_comm.
+      rewrite Raxioms.Rmult_1_l.
+      f_equal.
+      { repeat rewrite Raxioms.Rmult_assoc.
+        rewrite Raxioms.Rmult_comm.
+        f_equal.
+        rewrite <- Raxioms.Rmult_assoc.
+        rewrite Raxioms.Rmult_comm.
+        rewrite <- Raxioms.Rmult_assoc.
+        rewrite RIneq.Rinv_r; auto.
+        psatzl R. }
+      { repeat rewrite Raxioms.Rmult_assoc.
+        rewrite Raxioms.Rmult_comm.
+        f_equal.
+        rewrite <- Raxioms.Rmult_assoc.
+        rewrite Raxioms.Rmult_comm.
+        rewrite <- Raxioms.Rmult_assoc.
+        rewrite RIneq.Rinv_r; auto.
+        psatzl R. } }
+    { rewrite RIneq.Rinv_mult_distr; try assumption.
+      psatzl R. }
+    { repeat split; try assumption.
+      simpl. eapply RIneq.Rmult_integral_contrapositive; tauto. }
+    { eapply RIneq.Rmult_neq_0_reg in H0. destruct H0.
+      rewrite RIneq.Rinv_mult_distr; eauto.
+      rewrite RIneq.Rinv_involutive; eauto. psatzl R. }
+    { eapply RIneq.Rmult_neq_0_reg in H0. destruct H0.
+      tauto. }
+    { simpl. split; auto; split; auto. psatzl R. }
+    { simpl. split; auto; split; auto. psatzl R. }
+    { simpl. split; auto; split; auto. psatzl R. }
   Qed.
 End denote.
 
