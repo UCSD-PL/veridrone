af33042c5e8e310d2d8a49e35b0e1be1af9161d0
Mario Alvarez <mmcgil2@gmail.com>
2015-06-05 14:50:46 -0700

    Some work on rewriting rules in Postprocess.v; I'm still trying to understand how to use rewrite_strat though

--- compile/Postprocess.v
+++ compile/Postprocess.v
@@ -13,6 +13,7 @@ Require Import Coq.Reals.Rbase.
 Require Import Z3.Tactic.
 Require Import Abstractor.
 Require Import TLA.Automation.
+Require Import Coq.Classes.Morphisms.
 
 Lemma Z3Test : forall (a : R), (a + 1 = 3)%R%type -> ((a + 2 = 3)%R /\ ((1+1)%R=2%R)%R)%type.
 Proof.
@@ -108,6 +109,160 @@ Proof.
   shatterAbstraction. tauto.
 Qed.
 
+Lemma embed_push_Const : forall P, Embed (fun _ _ => P) -|- PropF P.
+Proof.
+  shatterAbstraction; tlaIntuition.
+Qed.
+
+(* Relation expressing a side-condition that must be used to push embed through arithmetic facts *)
+Definition evals_to (f : Term) (f' : state -> state -> R) : Prop :=
+  (eval_term f = f')%type.
+
+Notation "f =|> g" := (evals_to f g) (at level 60).
+
+(* comparison pushing *)
+Lemma embed_push_Eq :
+  forall l r l' r',
+    l =|> l' ->
+    r =|> r' ->
+    Embed (fun x y => l' x y = r' x y)%type -|-
+          Comp l r Eq.
+Proof.
+  intros.
+  unfold evals_to in *. 
+  shatterAbstraction. subst. reflexivity.
+Qed.
+
+Lemma embed_push_Gt :
+  forall l r l' r',
+    l =|> l' ->
+    r =|> r' ->
+    Embed (fun x y => Rgt (l' x y) (r' x y))%type -|-
+          Comp l r Gt.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Lemma embed_push_Le :
+  forall l r l' r',
+    l =|> l' ->
+    r =|> r' ->
+    Embed (fun x y => Rle (l' x y) (r' x y))%type -|-
+          Comp l r Le.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+(* arithmetic pushing *)
+Lemma arith_push_plus :
+  forall l r l' r',
+    l =|> l' ->
+    r =|> r' ->
+    PlusT l r =|> (fun x y => l' x y + r' x y)%R.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Lemma arith_push_minus :
+  forall l r l' r',
+    l =|> l' ->
+    r =|> r' ->
+    MinusT l r =|> (fun x y => l' x y - r' x y)%R.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Lemma arith_push_mult :
+  forall l r l' r',
+    l =|> l' ->
+    r =|> r' ->
+    MultT l r =|> (fun x y => l' x y * r' x y)%R.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Lemma arith_push_inv :
+  forall l l',
+    l =|> l' ->
+    InvT l =|> (fun x y => / (l' x y))%R.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Print Semantics.eval_term.
+
+Lemma arith_push_cos :
+  forall l l',
+    l =|> l' ->
+    CosT l =|> (fun x y => Rtrigo_def.cos (l' x y))%R.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Lemma arith_push_sin :
+  forall l l',
+    l =|> l' ->
+    SinT l =|> (fun x y => Rtrigo_def.sin (l' x y))%R.
+Proof.
+  intros. unfold evals_to in *. shatterAbstraction. subst. reflexivity.
+Qed.
+
+Print Syntax.Term.
+
+(* var, real *)
+Lemma arith_push_VarNow :
+  forall (x : Var),
+    VarNowT x =|> fun st _ => st x.
+Proof.
+  reflexivity.
+Qed.
+
+Lemma arith_push_VarNext :
+  forall (x : Var),
+    VarNextT x =|> fun _ st' => st' x.
+Proof.
+  reflexivity.
+Qed.
+
+(* special cases for 0 and 1: want to compile these into nats,
+   since nat are easier to work with *)
+Lemma arith_push_Nat_zero :
+    NatT 0 =|> fun _ _ => 0%R.
+Proof. reflexivity. Qed.
+
+Lemma arith_push_Nat_one :
+    NatT 1 =|> fun _ _ => 1%R.
+Proof. reflexivity. Qed.
+
+Lemma arith_push_Nat :
+  forall (n : nat),
+    NatT n =|> fun _ _ => INR n.
+Proof. reflexivity. Qed.
+
+Lemma arith_push_Real :
+  forall (r : R),
+    RealT r =|> fun _ _ => r.
+Proof. reflexivity. Qed.
+
+Create HintDb embed_push discriminated.
+Create HintDb arith_push discriminated.
+
+Hint Rewrite
+     embed_push_TRUE embed_push_FALSE
+     embed_push_And embed_push_Or embed_push_Imp
+     embed_push_Exists embed_push_Forall
+     embed_push_Const
+  : embed_push.
+
+Hint Rewrite
+     embed_push_Le embed_push_Eq embed_push_Gt embed_push_Le (* embed_push_Gt embed_push_Ge embed_push_Le *)
+     using solve [eauto 20 with arith_push]
+                         : embed_push.
+
+Hint Resolve
+     arith_push_plus arith_push_VarNow arith_push_VarNext arith_push_Nat_zero arith_push_Nat_one arith_push_Nat arith_push_Real
+  : arith_push.
+
 (* Useful lemmas *)
 Lemma AnyOf_singleton :
   forall (P : Prop), AnyOf [P] -|- P.
@@ -150,6 +305,100 @@ Proof.
   shatterAbstraction. tauto.
 Qed.
 
+(* Very simple program for testing. We want to prove that x stays >0 *)
+Definition float_one := source.nat_to_float 1.
+Opaque float_one.
+Definition simple_prog : fcmd :=
+  FAsn "x" (FPlus (FConst float_one) (FVar "x")).
+
+Definition simple_prog_ivs : list (Var * Var) :=
+  [("x", "x")].
+
+Lemma PropF_revert :
+  forall (P : Prop) (G Q : Syntax.Formula),
+    (P -> G |-- Q) ->
+    (G |-- PropF P -->> Q).
+Proof.
+  tlaIntuition.
+Qed.
+
+Lemma PropF_pull :
+  forall (P : Prop) (G Q : Syntax.Formula),
+    P ->
+    (G |-- Q) ->
+    (PropF P -->> G |-- Q).
+Proof.
+  tlaIntuition.
+Qed.
+
+(* Version of HoareA_embed_ex we can use for rewriting. *)
+Lemma HoareA_embed_ex_rw :
+  forall (c : fcmd)
+         (vs1 : list (Var * Var)),
+    var_spec_valid' vs1 ->
+    varmap_check_fcmd vs1 c ->
+    oembed_fcmd vs1 vs1 c |--
+                Forall Q : state -> Prop,
+  let P := fwp c Q vs1 in
+  PropF (SEMR vs1 Q) -->> Embed (fun st st' : state => P st -> Q st').
+Proof.
+  intros. apply lforallR.
+  intro. simpl. restoreAbstraction.
+  apply PropF_revert. intro.
+  tlaRevert.
+  apply HoareA_embed_ex; auto.
+  apply fwp_correct; auto.
+Qed.
+
+Axiom Always_proper : Proper (lentails ==> lentails) Syntax.Always.
+Existing Instance Always_proper.
+
+Fact fwp_simple : |-- "x" > 0 //\\ [](oembed_fcmd simple_prog_ivs simple_prog_ivs simple_prog) -->> []("x" > 0).
+Proof.
+  erewrite -> HoareA_embed_ex_rw; [| solve [simpl; intuition] | solve [simpl; intuition; eauto]].
+  charge_intros.
+  eapply discr_indX.
+  { red; intuition. }
+  { charge_assumption. }
+  { charge_assumption. }
+  { SearchAbout land lentails.
+    Print ILogic.
+    (* rhs *)
+    rewrite landforallDL. eapply lforallL. instantiate (1 := (fun st => st "x" > 0)%R).
+    tlaRevert. apply PropF_pull.
+    - unfold SEMR. intros. SearchAbout vmodels. admit (* prove a lemma *).
+    - simpl fwp.
+      rewrite_strat (topdown (old_hints embed_push)).
+      rewrite_strat (topdown (repeat (terms embed_push_TRUE embed_push_FALSE
+                                            embed_push_And embed_push_Or embed_push_Imp
+                                            embed_push_Exists embed_push_Forall
+                                            embed_push_Const))).
+      rewrite_strat (topdown (repeat (old_hints embed_push))).
+
+      rewrite_strat (topdown (repeat (terms embed_push_Imp embed_push_And embed_push_Or))).
+
+      autorewrite with embed_push.
+      rewrite 
+      rewrite_strat (bottomup (repeat (terms embed_push_Imp embed_push_And embed_push_Or))).
+      setoid_rewrite embed_push_Imp.
+      
+      rewrite_strat bottomup embed_push_Imp.
+      rewrite_strat bottomup (repeat (hints embed_push)).
+
+    
+      apply H.
+    
+    breakAbstraction.
+
+    
+    
+
+    
+  simpl. intuition. eexists. left. reflexivity.
+        
+  specialize (fwp_correct simple_prog_ivs ivs 
+                    
+
 (* sample abstractor outputs *)
 Section fwp.
   Parameter st : state.
@@ -163,8 +412,10 @@ Section fwp.
     cbv beta iota delta [fwp proportional_controller].
     simpl.
     Print bound.isVarValid.
+    
     unfold Semantics.eval_comp.
     simpl.
+
 
     (* TODO may need to bolt on continuation support *)
     Unset Ltac Debug.
