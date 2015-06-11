74ef25eacef42bdadf32ff0068d81e7fffd5f742
Gregory Malecha <gmalecha@cs.harvard.edu>
2015-06-10 17:01:43 -0700

    don't use the private url to get Charge.

--- get-charge.sh
+++ get-charge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone git@github.com:jesper-bengtson/Charge.git
+git clone https://github.com/jesper-bengtson/Charge.git
 (cd Charge; git checkout a21ed562baf247c8bc5677fc55a594325799e193)
 cp Charge.vars Charge/Charge\!/CoqoonMakefile.vars
 (cd Charge/Charge\!/ ; ln -s CoqoonMakefile Makefile ; make -j3)
