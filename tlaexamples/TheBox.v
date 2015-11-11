873b082abbd17509db736c61e63e64823a03235c
Daniel Ricketts <daricket@cs.ucsd.edu>
2015-11-10 20:29:58 -0800

    Made the box in terms of x and z.

--- tlaexamples/TheBox.v
+++ tlaexamples/TheBox.v
@@ -16,7 +16,7 @@ Local Open Scope string_scope.
 
 Module Type BoxParams.
   Parameter ub_X : R.
-  Parameter ub_Y : R.
+  Parameter ub_Z : R.
 
   Parameter d : R.
   Axiom d_gt_0 : (d > 0)%R.
@@ -28,20 +28,20 @@ Module Type BoxParams.
   (* Gravitational constant *)
   Parameter g : R.
   Axiom g_gt_0 : (g > 0)%R.
-  Parameter amin_Y : R.
-  Axiom amin_Y_lt_0 : (amin_Y < 0)%R.
-  Axiom amin_Y_lt_g : (amin_Y > -g)%R.
+  Parameter amin_Z : R.
+  Axiom amin_Z_lt_0 : (amin_Z < 0)%R.
+  Axiom amin_Z_lt_g : (amin_Z > -g)%R.
   Definition amin_X :=
-    ((amin_Y + g) * tan theta_min)%R.
+    ((amin_Z + g) * tan theta_min)%R.
 
   Parameter ubv_X : R.
-  Parameter ubv_Y : R.
+  Parameter ubv_Z : R.
   Axiom ub_ubv_X :
     (ubv_X*d + ubv_X*ubv_X*(0 - /2)*(/amin_X) <= ub_X)%R.
-  Axiom ub_ubv_Y :
-    (ubv_Y*d + ubv_Y*ubv_Y*(0 - /2)*(/amin_Y) <= ub_Y)%R.
+  Axiom ub_ubv_Z :
+    (ubv_Z*d + ubv_Z*ubv_Z*(0 - /2)*(/amin_Z) <= ub_Z)%R.
   Axiom ubv_X_gt_amin_d : (ubv_X >= -amin_X*d)%R.
-  Axiom ubv_Y_gt_amin_d : (ubv_Y >= -amin_Y*d)%R.
+  Axiom ubv_Z_gt_amin_d : (ubv_Z >= -amin_Z*d)%R.
 
 End BoxParams.
 
@@ -60,9 +60,9 @@ Module Box (P : BoxParams).
       assert (tan P.theta_min < 0)%R
       by (pose proof P.theta_min_bound;
           apply tan_lt_0; solve_linear).
-      pose proof P.amin_Y_lt_0.
+      pose proof P.amin_Z_lt_0.
       pose proof P.g_gt_0.
-      pose proof P.amin_Y_lt_g.
+      pose proof P.amin_Z_lt_g.
       solve_nonlinear.
     Qed.
     Definition ubv := P.ubv_X.
@@ -70,22 +70,22 @@ Module Box (P : BoxParams).
     Definition ubv_gt_amin_d := P.ubv_X_gt_amin_d.
   End X_Params.
 
-  Module Y_Params <: UpperLowerParams.
-    Definition ub := P.ub_Y.
+  Module Z_Params <: UpperLowerParams.
+    Definition ub := P.ub_Z.
     Definition d := P.d.
     Definition d_gt_0 := P.d_gt_0.
-    Definition amin := P.amin_Y.
-    Definition amin_lt_0 := P.amin_Y_lt_0.
-    Definition ubv := P.ubv_Y.
-    Definition ub_ubv := P.ub_ubv_Y.
-    Definition ubv_gt_amin_d := P.ubv_Y_gt_amin_d.
-  End Y_Params.
+    Definition amin := P.amin_Z.
+    Definition amin_lt_0 := P.amin_Z_lt_0.
+    Definition ubv := P.ubv_Z.
+    Definition ub_ubv := P.ub_ubv_Z.
+    Definition ubv_gt_amin_d := P.ubv_Z_gt_amin_d.
+  End Z_Params.
 
   Module X := UpperLower X_Params.
-  Module Y := UpperLower Y_Params.
+  Module Z := UpperLower Z_Params.
 
-  Let rename_y : RenameList :=
-    {{ "y" ~> "y" ; "v" ~> "vy" ; "a" ~> "ay" }}%rn.
+  Let rename_z : RenameList :=
+    {{ "y" ~> "z" ; "v" ~> "vz" ; "a" ~> "az" }}%rn.
 
   Let rename_x : RenameList :=
     {{ "y" ~> "x" ; "v" ~> "vx" ; "a" ~> "ax" }}%rn.
@@ -95,12 +95,12 @@ Module Box (P : BoxParams).
      constraints on control inputs. *)
   Definition NextRect : ActionFormula :=
     SysCompose (SysRename rename_x (deriv_term_RenameList rename_x) X.Next)
-               (SysRename rename_y (deriv_term_RenameList rename_y) Y.Next).
+               (SysRename rename_z (deriv_term_RenameList rename_z) Z.Next).
 
   (* theta is the angle between the y axis and
      the a vector *)
   Let rename_polar : RenameList :=
-    {{ "ax" ~> ("a"*sin("theta")) ; "ay" ~> ("a"*cos("theta") - P.g) }}%rn.
+    {{ "ax" ~> ("a"*sin("theta")) ; "az" ~> ("a"*cos("theta") - P.g) }}%rn.
 
   (* The system in polar coordinates, without the
      constraint on theta. *)
@@ -117,7 +117,7 @@ Module Box (P : BoxParams).
 
   Definition IndInv : ActionFormula :=
     Rename rename_polar (Rename rename_x X.IndInv //\\
-                         Rename rename_y Y.IndInv).
+                         Rename rename_z Z.IndInv).
 
   Transparent ILInsts.ILFun_Ops.
 
@@ -143,7 +143,7 @@ Module Box (P : BoxParams).
     { rewrite Sys_rename_formula...
       rewrite SysRename_rule...
       rewrite TimedPreserves_Rename...
-      rewrite <- Y.TimedPreserves_Next.
+      rewrite <- Z.TimedPreserves_Next.
       Rename_rewrite. charge_tauto. }
   Qed.
 
@@ -156,18 +156,18 @@ Definition shift (ub lb:R) (x:Var) : list (Var*Term) :=
 Variable ubx lbx uby lby ubvx ubvy lbvx lbvy : R.
 
 Goal (Rename (to_RenameMap (shift ubx lbx "x"))
-        (Rename (to_RenameMap (shift uby lby "y"))
+        (Rename (to_RenameMap (shift uby lby "z"))
            (Rename (to_RenameMap (shift ubvx lbvx "vx"))
               (Rename (to_RenameMap (shift ubvy lbvy "vy"))
                       (Prog SpecR))))) |-- TRUE.
   unfold SpecR. rewrite_rename_pf SpecPolarR.
   rewrite Prog_SysCompose. rewrite Prog_SysRename.
   unfold SpecRectR. rewrite_rename_pf X_SpecR.
-  rewrite_rename_pf Y_SpecR. rewrite Prog_SysCompose.
-  repeat rewrite Prog_SysRename. unfold X.SpecR, Y.SpecR.
+  rewrite_rename_pf Z_SpecR. rewrite Prog_SysCompose.
+  repeat rewrite Prog_SysRename. unfold X.SpecR, Z.SpecR.
   repeat rewrite Prog_SysCompose.
   unfold X.Vel.SpecR, X.Position.SpecR,
-  Y.Vel.SpecR, Y.Position.SpecR.
+  Z.Vel.SpecR, Z.Position.SpecR.
   repeat rewrite Prog_SysCompose.
   repeat match goal with
          |- context [ projT1 ?X ]
@@ -182,24 +182,24 @@ Goal (Rename (to_RenameMap (shift ubx lbx "x"))
   |- context [ Unchanged ?l ] =>
     rewrite ltrueR with (C:=Unchanged l)
   end; repeat rewrite landtrueR.
-  unfold X.Position.Monitor.Ctrl, Y.Position.Monitor.Ctrl,
-  X.Vel.Vel.Ctrl, Y.Vel.Vel.Ctrl.
+  unfold X.Position.Monitor.Ctrl, Z.Position.Monitor.Ctrl,
+  X.Vel.Vel.Ctrl, Z.Vel.Vel.Ctrl.
   Rename_rewrite.
   repeat rewrite X.Vel.Vel.Rename_SafeAcc
     by rw_side_condition.
   repeat rewrite X.Vel.Vel.Rename_Default
     by rw_side_condition.
-  repeat rewrite Y.Vel.Vel.Rename_SafeAcc
+  repeat rewrite Z.Vel.Vel.Rename_SafeAcc
     by rw_side_condition.
-  repeat rewrite Y.Vel.Vel.Rename_Default
+  repeat rewrite Z.Vel.Vel.Rename_Default
     by rw_side_condition.
   repeat rewrite X.Position.Monitor.Rename_SafeAcc
     by rw_side_condition.
   repeat rewrite X.Position.Monitor.Rename_Default
     by rw_side_condition.
-  repeat rewrite Y.Position.Monitor.Rename_SafeAcc
+  repeat rewrite Z.Position.Monitor.Rename_SafeAcc
     by rw_side_condition.
-  repeat rewrite Y.Position.Monitor.Rename_Default
+  repeat rewrite Z.Position.Monitor.Rename_Default
     by rw_side_condition.
   simpl rename_term.
   (* Get rid of history variables with no computational
@@ -221,28 +221,28 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
   (* Now we move on to Enabled *)
 
   Definition InputConstraintRect : Formula :=
-     "ay" + P.g > 0 //\\
-     P.theta_min <= ArctanT ("ax"/("ay" + P.g))
+     "az" + P.g > 0 //\\
+     P.theta_min <= ArctanT ("ax"/("az" + P.g))
                  <= P.theta_max.
 
-  Definition XYConstraint :=
+  Definition XZConstraint :=
     Rename rename_x X.Constraint //\\
-    Rename rename_y Y.Constraint.
+    Rename rename_z Z.Constraint.
 
   Lemma xy_constraint_refinement :
-    XYConstraint |-- InputConstraintRect.
+    XZConstraint |-- InputConstraintRect.
   Proof.
-    unfold XYConstraint.
+    unfold XZConstraint.
     repeat rewrite <- Rename_ok by rw_side_condition.
     breakAbstraction. intros.
     generalize dependent (Stream.hd tr). intros.
-    unfold X_Params.amin, Y_Params.amin, P.amin_X in *.
+    unfold X_Params.amin, Z_Params.amin, P.amin_X in *.
     assert (tan P.theta_min < 0)%R
       by (pose proof P.theta_min_bound;
           apply tan_lt_0; solve_linear).
     pose proof P.theta_min_bound.
-    pose proof P.amin_Y_lt_0.
-    pose proof P.amin_Y_lt_g.
+    pose proof P.amin_Z_lt_0.
+    pose proof P.amin_Z_lt_g.
     pose proof P.g_gt_0.
     unfold P.theta_max.
     split; [ solve_linear | ].
@@ -291,7 +291,7 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
          specialize (H _ H0); discriminate).
   Qed.
 
-  Lemma y_witness_function :
+  Lemma z_witness_function :
     exists f,
     forall xs,
       List.forallb
@@ -302,11 +302,11 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
                              String.string_dec "Y"
                              (List.remove
                                 String.string_dec "y"
-                                (get_image_vars rename_y)))))
+                                (get_image_vars rename_z)))))
         xs = true ->
-      witness_function (to_RenameMap rename_y) f xs.
+      witness_function (to_RenameMap rename_z) f xs.
   Proof.
-    exists (get_witness rename_y).
+    exists (get_witness rename_z).
     intros. simpl. unfold witness_function. intros.
     repeat (destruct_ite; simpl); subst; try reflexivity;
     try (rewrite List.forallb_forall in H;
@@ -329,7 +329,7 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
       (fun st x =>
          let witness :=
              proj1_sig
-               (rectangular_to_polar (st "ay" + P.g)
+               (rectangular_to_polar (st "az" + P.g)
                                      (st "ax")) in
          if String.string_dec x "a"
          then fst witness
@@ -346,10 +346,10 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
              | [ |- context [if ?e then _ else _] ]
                => destruct e; simpl
              end; subst; simpl.
-      { destruct (rectangular_to_polar (st "ay" + P.g)
+      { destruct (rectangular_to_polar (st "az" + P.g)
                                        (st "ax")).
         simpl. tauto. }
-      { destruct (rectangular_to_polar (st "ay" + P.g)
+      { destruct (rectangular_to_polar (st "az" + P.g)
                                        (st "ax")).
         simpl. unfold Value. solve_linear. }
       { destruct (String.string_dec "a" "a");
@@ -358,7 +358,7 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
         intuition congruence. }
       { reflexivity. } }
     { intros. destruct tr. simpl.
-      destruct (rectangular_to_polar (s "ay" + P.g)
+      destruct (rectangular_to_polar (s "az" + P.g)
                                      (s "ax")).
       simpl. solve_linear. }
   Qed.
@@ -375,8 +375,8 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
       by (pose proof P.theta_min_bound;
           apply tan_lt_0; solve_linear).
     pose proof P.theta_min_bound.
-    pose proof P.amin_Y_lt_0.
-    pose proof P.amin_Y_lt_g.
+    pose proof P.amin_Z_lt_0.
+    pose proof P.amin_Z_lt_g.
     pose proof P.g_gt_0.
     unfold P.theta_max in *.
     decompose [and] H. clear H.
@@ -456,16 +456,16 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
             apply next_st_formula_entails in H;
               [ | simpl; tauto | simpl; tauto ].
             rewrite <- H. clear.
-            unfold XYConstraint. rewrite next_And.
+            unfold XZConstraint. rewrite next_And.
             (* Very brittle match ahead. Basically
                just want to group the X monitor
-               with the X constraint and the Y monitor
-               with the Y constraint. *)
+               with the X constraint and the Z monitor
+               with the Z constraint. *)
             repeat rewrite landA.
             match goal with
-            |- _ |-- Enabled (?X //\\ ?Y //\\ ?XC //\\ ?YC) =>
-            assert (X //\\ Y //\\ XC //\\ YC -|-
-                    (X //\\ XC) //\\ (Y //\\ YC))
+            |- _ |-- Enabled (?X //\\ ?Z //\\ ?XC //\\ ?ZC) =>
+            assert (X //\\ Z //\\ XC //\\ ZC -|-
+                    (X //\\ XC) //\\ (Z //\\ ZC))
               as H by (split; charge_tauto);
               rewrite H; clear H
             end.
@@ -483,14 +483,14 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
                   etransitivity; [ apply H |
                                    apply Proper_Enabled_lentails ].
                   charge_tauto. } }
-              { pose proof y_witness_function. destruct H.
+              { pose proof z_witness_function. destruct H.
                 rewrite next_Rename.
                 rewrite Rename_ok by eauto with rw_rename.
                 rewrite <- Rename_and. apply landL2.
                 eapply subst_enabled with (f:=x).
                 { apply get_vars_next_state_vars; reflexivity. }
                 { apply H; reflexivity. }
-                { clear. pose proof Y.SysNeverStuck_Discr.
+                { clear. pose proof Z.SysNeverStuck_Discr.
                   charge_clear.
                   etransitivity; [ apply H |
                                    apply Proper_Enabled_lentails ].
@@ -506,7 +506,7 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
 
   Definition Safe : StateFormula :=
     Rename rename_polar
-           (Rename rename_x X.Safe //\\ Rename rename_y Y.Safe).
+           (Rename rename_x X.Safe //\\ Rename rename_z Z.Safe).
 
   Lemma IndInv_impl_Safe : IndInv //\\ TimeBound P.d |-- Safe.
   Proof with (eauto with rw_rename).
@@ -520,12 +520,12 @@ SecondDerivUtil.tdist "vx" ("a"!*sin("theta"!)) P.d = 0.
       repeat rewrite <- (Rename_ok _ rename_x)...
       repeat rewrite <- Rename_ok...
       simpl. restoreAbstraction. charge_tauto. }
-    { rewrite <- Y.IndInv_impl_Safe.
+    { rewrite <- Z.IndInv_impl_Safe.
       unfold IndInv, TimeBound.
       repeat rewrite Rename_and.
       charge_split; [ charge_assumption | ].
       charge_revert. charge_clear.
-      repeat rewrite <- (Rename_ok _ rename_y)...
+      repeat rewrite <- (Rename_ok _ rename_z)...
       repeat rewrite <- Rename_ok...
       simpl. restoreAbstraction. charge_tauto. }
   Qed.
