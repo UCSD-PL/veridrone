14aac002d946ec1ee707d8eb6697f23764b15f8d
Gregory Malecha <gmalecha@cs.harvard.edu>
2015-11-12 13:28:46 -0800

    attempting a proof of the quadcopter model.

--- tlaexamples/Quadcopter.v
+++ tlaexamples/Quadcopter.v
@@ -93,9 +93,61 @@ Section quadcopter.
                (Continuous W_quadcopter_plane).
   Proof.
     intros.
-    rewrite <- Rename_Continuous_deriv_term'.
-    2: eapply deriv_term_list.
-    (* This is a quite complex proof due to the incompleteness of the theory. *)
+    rewrite <- Rename_Continuous'.
+    Require Import Coq.Reals.Ratan.
+    instantiate (1 := fun st' x =>
+                        let ax' := st' "a" * cos(pitch) * sin(roll) +
+                                   "a" * (0 - sin(pitch)) * sin(roll) +
+                                   "a" * cos(pitch) * cos(roll) in
+                        let ay' := st' "a" * sin(pitch) + "a" * cos(pitch) in
+                        if String.string_dec x "a" then
+                          / (2 * SqrtT (ax * ax + ay * ay)) * (2 * ax * ax' + 2 * ay * ay')
+                        else if String.string_dec x "theta" then
+                               let u' := 2 * ay / ax * (ax * ay' - ay * ax') / (ax * ax) in
+                                / (1 + (ay / ax) * (ay / ax)) * u'
+                             else
+                               st' x).
+    { eapply Proper_Continuous_entails.
+      red; intros.
+      charge_intros.
+      rewrite <- Rename_ok by eauto with rw_rename.
+      simpl; restoreAbstraction.
+      unfold W_quadcopter.
+      unfold small_angle_constraint.
+      breakAbstraction. intros.
+      forward_reason.
+      repeat rewrite H0; simpl; clear H0.
+      destruct H.
+      { admit. }
+      forward_reason.
+      Cases.rewrite_all_goal.
+      rewrite_real_zeros.
+      pose (cos (Stream.hd tr pitch)) as cpitch; fold cpitch.
+      pose (sin (Stream.hd tr pitch)) as spitch; fold spitch.
+      pose (cos (Stream.hd tr roll)) as croll; fold croll.
+      pose (sin (Stream.hd tr roll)) as sroll; fold sroll.
+      generalize dependent (Stream.hd tr "a"). intro a_; intros.
+      cutrewrite (eq (sqrt (a_ * cpitch * sroll *
+                        (a_ * cpitch * sroll) +
+                        a_ * spitch * (a_ * spitch)))
+                     (a_ * sqrt (cpitch * cpitch * sroll * sroll + spitch * spitch)))%R.
+      { repeat split; auto.
+        { rewrite ArithFacts.cos_atan.
+          cutrewrite (eq (1 +
+                          a_ * spitch * / (a_ * cpitch * sroll) *
+                          (a_ * spitch * / (a_ * cpitch * sroll)))
+                         (1 + (spitch / (cpitch * sroll) * (spitch / (cpitch * sroll)))))%R.
+          { repeat rewrite Rmult_assoc.
+            admit. }
+          { admit. } }
+        { rewrite ArithFacts.sin_atan.
+          admit. }
+        { cutrewrite (eq (a_ * cpitch * sroll * (a_ * cpitch) -
+                          a_ * spitch * (a_ * (0 - spitch) * sroll + a_ * cpitch * croll))
+                         0)%R.
+          { rewrite_real_zeros. reflexivity. }
+          { ring_simplify. (* NOT WORKING *)
+    (* This is a quite complex proof. *)
   Abort.
 
   Definition quadcopter : ActionFormula :=
