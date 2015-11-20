1644ef7db8cacbc752e9c23a2abae0fe4d748e31
Daniel Ricketts <daricket@cs.ucsd.edu>
2015-11-20 13:28:42 -0800

    Removing irrelevant files.

--- examples/Quadcopter.v
+++ examples/Quadcopter.v
@@ -1,8 +1,8 @@
 Require Import Coq.Reals.Reals.
 Require Import ExtLib.Tactics.
-Require Import TLA.TLA.
-Require Import TLA.ProofRules.
-Require Import Examples.System2.
+Require Import Logic.Logic.
+Require Import Logic.ProofRules.
+Require Import Examples.System.
 
 Set Implicit Arguments.
 Set Strict Implicit.
@@ -80,80 +80,5 @@ Section quadcopter.
     rewrite H10.
     rewrite_real_zeros. reflexivity.
   Qed.
-
-  Theorem W_quadcopter_plane_refinement
-  : let ax : Term := "a" * cos( pitch ) * sin( roll ) in
-    let ay : Term := "a" * sin( pitch ) in
-    let az : Term := "a" * cos( pitch ) * cos( roll ) in
-    let A : Term := SqrtT (ax * ax + ay * ay) in
-    let theta : Term := ArctanT (ay / ax) in
-        Continuous W_quadcopter
-    |-- Rename ({{ "a" ~> A
-                 & "theta" ~> theta }})%rn
-               (Continuous W_quadcopter_plane).
-  Proof.
-    intros.
-    rewrite <- Rename_Continuous'.
-    Require Import Coq.Reals.Ratan.
-    instantiate (1 := fun st' x =>
-                        let ax' := st' "a" * cos(pitch) * sin(roll) +
-                                   "a" * (0 - sin(pitch)) * sin(roll) +
-                                   "a" * cos(pitch) * cos(roll) in
-                        let ay' := st' "a" * sin(pitch) + "a" * cos(pitch) in
-                        if String.string_dec x "a" then
-                          / (2 * SqrtT (ax * ax + ay * ay)) * (2 * ax * ax' + 2 * ay * ay')
-                        else if String.string_dec x "theta" then
-                               let u' := 2 * ay / ax * (ax * ay' - ay * ax') / (ax * ax) in
-                                / (1 + (ay / ax) * (ay / ax)) * u'
-                             else
-                               st' x).
-    { eapply Proper_Continuous_entails.
-      red; intros.
-      charge_intros.
-      rewrite <- Rename_ok by eauto with rw_rename.
-      simpl; restoreAbstraction.
-      unfold W_quadcopter.
-      unfold small_angle_constraint.
-      breakAbstraction. intros.
-      forward_reason.
-      repeat rewrite H0; simpl; clear H0.
-      destruct H.
-      { admit. }
-      forward_reason.
-      Cases.rewrite_all_goal.
-      rewrite_real_zeros.
-      pose (cos (Stream.hd tr pitch)) as cpitch; fold cpitch.
-      pose (sin (Stream.hd tr pitch)) as spitch; fold spitch.
-      pose (cos (Stream.hd tr roll)) as croll; fold croll.
-      pose (sin (Stream.hd tr roll)) as sroll; fold sroll.
-      generalize dependent (Stream.hd tr "a"). intro a_; intros.
-      cutrewrite (eq (sqrt (a_ * cpitch * sroll *
-                        (a_ * cpitch * sroll) +
-                        a_ * spitch * (a_ * spitch)))
-                     (a_ * sqrt (cpitch * cpitch * sroll * sroll + spitch * spitch)))%R.
-      { repeat split; auto.
-        { rewrite ArithFacts.cos_atan.
-          cutrewrite (eq (1 +
-                          a_ * spitch * / (a_ * cpitch * sroll) *
-                          (a_ * spitch * / (a_ * cpitch * sroll)))
-                         (1 + (spitch / (cpitch * sroll) * (spitch / (cpitch * sroll)))))%R.
-          { repeat rewrite Rmult_assoc.
-            admit. }
-          { admit. } }
-        { rewrite ArithFacts.sin_atan.
-          admit. }
-        { cutrewrite (eq (a_ * cpitch * sroll * (a_ * cpitch) -
-                          a_ * spitch * (a_ * (0 - spitch) * sroll + a_ * cpitch * croll))
-                         0)%R.
-          { rewrite_real_zeros. reflexivity. }
-          { ring_simplify. (* NOT WORKING *)
-    (* This is a quite complex proof. *)
-  Abort.
-
-  Definition quadcopter : ActionFormula :=
-    Sys D W_quadcopter delta.
-
-  Definition Quadcopter : ActionFormula :=
-    System D W_quadcopter delta.
 
 End quadcopter.
